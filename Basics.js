4fc7e157cc34a270b4e7a8789806c9f876a017ad
ArtFK <55387235+ArtFK@users.noreply.github.com>
2019-11-14 16:58:08 +0100

    Add files via upload

---
+++ Basics.js
@@ -0,0 +1,5 @@
+var nombre_apellidos = "Arturo Fernandez Knieling"
+var miEdad= 30;
+var díasSemana= ["lunes", "martes", "miercoles", "jueves", "viernes"]
+var Gravedad= [9.8 m/s2];
+var impares= 1,3,7;
